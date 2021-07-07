c2e26c32d3d87e1b83508b199e6879d456bf6453
Emmirald289 <emmirald@gmail.com>
2021-07-07 19:39:46 +0100

    Testing environment variables

--- lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -1,11 +1,13 @@
 
+
+const favorite_food = process.env.FAVORITE_FOOD;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`Microservices rock! And my favorite food is ${favorite_food}`);
     await sleep(5000);
   }
 }
